9f36fd075744cb08786dd87f6fb48daff7b737b9
CyferShepard <govenderthegan@gmail.com>
2025-06-12 11:04:24 +0200

    update import path for web scraper API to use the latest version

--- src/classes/api-parser.ts
+++ src/classes/api-parser.ts
@@ -6,6 +6,6 @@ import {
   parseQuery,
   HTTPMethod,
   BodyType,
-} from "../../../../deno/api-parser/mod.ts";
+} from "https://deno.land/x/web_scraper_api@v1.0.0/mod.ts";
 
 export { ScraperPayload, ScraperResponse, ScraperQuery, ScraperRegex, parseQuery, HTTPMethod, BodyType };
