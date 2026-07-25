8a99042bbc89bda9f7d7e1441df53da3b445b7ca
CyferShepard <govenderthegan@gmail.com>
2026-07-25 23:22:54 +0200

    fix: enable force loading of parsers during plugin update

--- src/routes/api.ts
+++ src/routes/api.ts
@@ -284,7 +284,7 @@ apiRouter.get("/updatePlugins", authMiddleware, async (context) => {
       "main", // branch
       "./src/plugins", // local destinationto send messages
     );
-    await parserRegistry.loadParsers();
+    await parserRegistry.loadParsers(true);
   } catch (e) {
     console.error("Error updating plugins:", e);
     broadcastMessage(new Map().set("type", "updatePlugins").set("message", `Error updating plugins`));
